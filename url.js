97babc0bc4eea58759d2b392f95db44d90154133
Ranveer098 <95100082+Ranveer098@users.noreply.github.com>
2024-06-11 14:14:42 +0530

    initial Commit

---
+++ url.js
@@ -0,0 +1,3 @@
+// module.exports = `mongodb://localhost:27017`
+// module.exports = "mongodb+srv://admin:admin123.vtkja.mongodb.net/"
+module.exports="mongodb+srv://admin:admin123@cluster0.u94fgcx.mongodb.net/"
